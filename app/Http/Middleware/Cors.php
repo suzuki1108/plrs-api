cf3cfa517c7852511b2e7403e616cd3a186fd017
鈴木翔太 <shotasuzuki@suzukishoutanoMacBook-Pro.local>
2021-07-05 23:19:43 +0900

    corsテスト

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://programming-ranking.com/')
+            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
